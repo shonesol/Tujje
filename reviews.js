b4fab334e44ededa8247632e6d30f59df9cca90b
TAMALE SHONE <70106850+shonesol@users.noreply.github.com>
2026-07-09 20:21:17 +0300

    Update reviews.js

--- reviews.js
+++ reviews.js
@@ -1,150 +1,85 @@
-import {initializeApp}
-from
+import { initializeApp } from 
 "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
-
 import {
-
 getFirestore,
 collection,
 addDoc,
+getDocs,
+query,
+orderBy,
 serverTimestamp
-
-}
-
-from
-
+} from 
 "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
-
-
 
 
 const firebaseConfig = {
 
-apiKey:"AIzaSyAUxwj4JVPWAz3oDjPTSaEUot49pHxmWQg",
+apiKey: "AIzaSyAUxwj4JVPWAz3oDjPTSaEUot49pHxmWQg",
 
-authDomain:"tujjesafari-dbda9.firebaseapp.com",
+authDomain: "tujjesafari-dbda9.firebaseapp.com",
 
-projectId:"tujjesafari-dbda9",
+projectId: "tujjesafari-dbda9",
 
-storageBucket:"tujjesafari-dbda9.firebasestorage.app",
+storageBucket: "tujjesafari-dbda9.firebasestorage.app",
 
-messagingSenderId:"39777982092",
+messagingSenderId: "39777982092",
 
-appId:"1:39777982092:web:bc488a73cf7bb637204da9"
+appId: "1:39777982092:web:bc488a73cf7bb637204da9"
 
 };
 
 
+const app = initializeApp(firebaseConfig);
 
-
-const app=initializeApp(firebaseConfig);
-
-const db=getFirestore(app);
-
+const db = getFirestore(app);
 
 
 
-
-window.submitReview=async function(){
-
-
-let name=document.getElementById("name").value;
-
-let country=document.getElementById("country").value;
-
-let rating=document.getElementById("rating").value;
-
-let review=document.getElementById("review").value;
+const box = document.getElementById("reviewBox");
 
 
 
-if(!name || !review){
+async function loadReviews(){
 
-alert("Please complete your review");
-
-return;
-
-}
-
+const q = query(
+collection(db,"reviews"),
+orderBy("date","desc")
+);
 
 
-try{
+const snap = await getDocs(q);
 
 
-await addDoc(collection(db,"reviews"),{
+box.innerHTML="";
 
 
-name:name,
+snap.forEach(doc=>{
 
-country:country,
 
-rating:Number(rating),
+let r = doc.data();
 
-review:review,
 
-date:serverTimestamp()
+box.innerHTML += `
 
+<div class="review-card">
+
+<h3>${r.name}</h3>
+
+<p>${"⭐".repeat(Number(r.rating))}</p>
+
+<p>${r.review}</p>
+
+<small>${r.country}</small>
+
+</div>
+
+`;
 
 });
 
 
-alert("⭐ Thank you for your review!");
-
-document.querySelector(".container").reset;
-
-
-}
-
-catch(error){
-
-console.log(error);
-
-alert("Error sending review");
-
 }
 
 
-};
-
-
-
-
-
-const reviewLink =
-"https://www.tujjesafari.com/review.html";
-
-
-
-window.shareWhatsApp=function(){
-
-window.open(
-"https://wa.me/?text="+
-encodeURIComponent(reviewLink)
-);
-
-}
-
-
-
-window.shareFacebook=function(){
-
-window.open(
-
-"https://www.facebook.com/sharer/sharer.php?u="+
-encodeURIComponent(reviewLink)
-
-);
-
-}
-
-
-
-window.copyLink=function(){
-
-navigator.clipboard.writeText(reviewLink);
-
-alert("Review link copied");
-
-}
+loadReviews();
